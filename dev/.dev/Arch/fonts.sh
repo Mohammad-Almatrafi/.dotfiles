78c752a7fb73af4ca4b4daf7ac95d0cd6579da91
Rur1k <moalmatrafi90@gmail.com>
2025-07-02 12:34:28 +0300

    added new fonts to install in dev Arch

--- dev/.dev/Arch/fonts.sh
+++ dev/.dev/Arch/fonts.sh
@@ -6,6 +6,8 @@ sudo pacman -S --noconfirm ttf-roboto
 sudo pacman -S --noconfirm ttf-roboto-mono
 sudo pacman -S --noconfirm ttf-roboto-mono-nerd
 sudo pacman -S --noconfirm ttf-cascadia-mono-nerd
+sudo pacman -S --noconfirm ttf-cascadia-code-nerd
+sudo pacman -S --noconfirm ttf-cascadia-code
 sudo pacman -S --noconfirm ttf-sarasa-gothic
 sudo pacman -S --noconfirm ttf-noto-nerd
 sudo pacman -S --noconfirm ttf-dejavu
